d11230d483d96bbc4c9a2c8d6ed5a12813672562
Spencer Denauski <oxideous@hotmail.com>
2015-07-19 00:04:20 -0400

    MAJOR PROGRESS.  CLOSING OUT SATURDAY NIGHT.  A LOT OF STUFF WORKS, STILL WAY MORE TO DO

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,188 @@
-/******************************************************************************
- * Playing Card Objects                                                        *
- *                                                                             *
- * Do not remove this notice.                                                  *
- *                                                                             *
- * Copyright 2001 by Mike Hall                                                 *
- * Please see http://www.brainjar.com for terms of use.                        *
- ******************************************************************************/
-
-//=============================================================================
-// Card Object
+//    var $dealerImg = $("<img>");
+//    $dealerImg.attr("src", cards[93].image);
+//    $dealerImg.attr("style", "max-height: 200px;");
+//    $('#dealer-area').append($dealerImg);
 //
-// Note: Requires cards.css for display.
-//=============================================================================
+//    var $playerImg = $("<img>");
+//    $playerImg.attr("src", cards[25].image);
+//    $playerImg.attr("style", "max-height: 200px;");
+//    $('#player-area').append($playerImg);
+
+//placebets
+//initaldealcards
+//roundfinished
+
+$(document).ready(function () {
+    startModal();
+});
+
+var bankroll = 1000,
+    totalBet = 0,
+    winnings = 0,
+    dealRoundCounter = 0,
+    dealTimeDelay = 10000,
+    $currentBet = $("#current-bet p"),
+    $bankrollDisplay = $("#bankroll-display p"),
+    $commonDisplay = $("#common-area h3"),
+    discardPile = [],
+    player = [],
+    dealer = [];
+
+function setStatus(currentStatus) {
+    console.log(currentStatus);
+    switch (currentStatus) {
+        case "roundInit":
+            stackShuffle(cards.length);
+            $("#dealer-actions li button").addClass("disabled");
+            placeBets();
+            break;
+        case "roundStart":
+            $("#dealer-actions #deal").removeClass("disabled");
+            $("#dealer-actions #deal").on("click", function () {
+                dealRoundCounter = 1;
+                $("#dealer-actions li button").addClass("disabled");
+                //stackAddCard(stackDeal());
+                $("#dealer-actions #deal").off("click");
+                $(".bet").off("click");
+                setStatus("dealCards");
+            });
+            break;
+        case "dealCards":
+            dealRound()
+            break;
+    }
+}
+
+
+function dealRound() {
+
+    // Deal a card to the player or the dealer based on the counter.
+    while (dealRoundCounter < 5) {
+        switch (dealRoundCounter) {
+            case 1:
+                stackAddCard(stackDeal(), player);
+                var pCard1 = $('<img id="p-card-1" class="card">');
+                pCard1.attr('src', player[0].image);
+                pCard1.appendTo('#player-area');
+                break;
+            case 2:
+                stackAddCard(stackDeal(), dealer);
+                var dCard1 = $('<img id="d-card-1" class="card">');
+                dCard1.attr('src', dealer[0].image);
+                dCard1.appendTo('#dealer-area');
+                break;
+            case 3:
+                stackAddCard(stackDeal(), player);
+                var pCard2 = $('<img id="p-card-2" class="card">');
+                pCard2.attr('src', player[1].image);
+                pCard2.appendTo('#player-area');
+                break;
+
+            case 4:
+                stackAddCard(stackDeal(), dealer);
+                var dCard2 = $('<img id="d-card-2" class="card">');
+                dCard2.attr('src', dealer[1].image);
+                dCard2.appendTo('#dealer-area');
+                break;
+
+                //        default:
+
+                // No more cards to deal, play the round.
+
+                //            playRound();
+                //            return;
+                //            break;
+        }
+
+        // Update the player's score.
+
+        //        if (player[0].getScore() == 21) {
+        ////            player[0].blackjack = true;
+        //            player[0].scoreTextNode.nodeValue = "Blackjack";
+        //        } else
+        //            player[0].scoreTextNode.nodeValue = player[0].getScore();
+
+        // Set a timer for the next call.
+
+        dealRoundCounter++;
+        setTimeout(dealRound, dealTimeDelay);
+
+
+    }
+}
+
+
+
+//function dealRound() {
+//    // Deal a card to the player or the dealer based on the counter.
+//    switch (dealRoundCounter) {
+//        case 1:
+//            player[0].stackAddCard(getNextCard(), false);
+//            break;
+//        case 2:
+//            dealer.stackAddCard(getNextCard(), true);
+//            break;
+//        case 3:
+//            player[0].stackAddCard(getNextCard(), false);
+//            break;
+//        case 4:
+//            dealer.stackAddCard(getNextCard(), false);
+//            break;
+//            //default:
+//            // No more cards to deal, play the round.
+//            //playRound();
+//            //return;
+//            //break;
+//    }
+//
+//    // Update the player's score.
+//    //    if (player[0].getScore() == 21) {
+//    //        player[0].blackjack = true;
+//    //        player[0].scoreTextNode.nodeValue = "Blackjack";
+//    //    }
+//    //    else
+//    //        player[0].scoreTextNode.nodeValue = player[0].getScore();
+//    //
+//    //    // Set a timer for the next call.
+//    //
+//    dealRoundCounter++;
+//    setTimeout(dealRound, dealTimeDelay);
+//}
+
+function startModal() {
+    $('#start-modal').foundation('reveal', 'open');
+    $("#start-modal .button").on('click', function () {
+        var numberOfDecks = $("#number-of-decks").val();
+        $('#start-modal').foundation('reveal', 'close');
+        $bankrollDisplay.html("$" + bankroll);
+        stackMakeDeck(numberOfDecks);
+        setStatus("roundInit");
+        return cards
+    });
+}
+
+function placeBets() {
+    var counter = 0;
+    $commonDisplay.html("Place your bets<br/>and click deal!");
+    $(".bet").on("click", function () {
+        var amount = $(this).val();
+        if (bankroll >= amount) {
+            totalBet += amount;
+            bankroll -= amount;
+            $currentBet.html("$" + totalBet);
+            $bankrollDisplay.html("$" + bankroll);
+            //return totalBet;
+            if (counter === 0) {
+                setStatus("roundStart");
+                counter += 1;
+            } else {
+                counter += 1;
+            }
+        } else {
+            alert("You do not have enough money!");
+        }
+    });
+}
 
 //-----------------------------------------------------------------------------
 // Card constructor function.
@@ -24,7 +195,7 @@ function Card(rank, suit, image) {
     this.image = image;
 
     this.toString = cardToString;
-    this.createNode = cardCreateNode;
+    //this.createNode = cardCreateNode;
 }
 
 //-----------------------------------------------------------------------------
@@ -119,161 +290,161 @@ function cardToString() {
 //var cardImg3 = new Image();
 //cardImg3.src = "img/cards/c01.png";
 
-function cardCreateNode() {
-
-    var cardNode, frontNode, indexNode, spotNode, tempNode, textNode;
-    var indexStr, spotChar;
-
-    // This is the main node, a DIV tag.
-
-    cardNode = document.createElement("DIV");
-    cardNode.className = "card";
-
-    // Build the front of card.
-
-    frontNode = document.createElement("DIV");
-    frontNode.className = "front";
-
-    // Get proper character for card suit and change font color if necessary.
-
-    spotChar = "\u00a0";
-    switch (this.suit) {
-        case "C":
-            spotChar = "\u2663";
-            break;
-        case "D":
-            frontNode.className += " red";
-            spotChar = "\u2666";
-            break;
-        case "H":
-            frontNode.className += " red";
-            spotChar = "\u2665";
-            break;
-        case "S":
-            spotChar = "\u2660";
-            break;
-    }
-
-    // Create and add the index (rank) to the upper-left corner of the card.
-
-    indexStr = this.rank;
-    if (this.toString() == "")
-        indexStr = "\u00a0";
-    spotNode = document.createElement("DIV");
-    spotNode.className = "index";
-    textNode = document.createTextNode(indexStr);
-    spotNode.appendChild(textNode);
-    spotNode.appendChild(document.createElement("BR"));
-    textNode = document.createTextNode(spotChar);
-    spotNode.appendChild(textNode);
-    frontNode.appendChild(spotNode);
-
-    // Create and add spots based on card rank (Ace thru 10).
-
-    spotNode = document.createElement("DIV");
-    textNode = document.createTextNode(spotChar);
-    spotNode.appendChild(textNode);
-    if (this.rank == "A") {
-        spotNode.className = "ace";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-    if (this.rank == "3" || this.rank == "5" || this.rank == "9") {
-        spotNode.className = "spotB3";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-    if (this.rank == "2" || this.rank == "3") {
-        spotNode.className = "spotB1";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-    if (this.rank == "2" || this.rank == "3") {
-        spotNode.className = "spotB5";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-    if (this.rank == "4" || this.rank == "5" || this.rank == "6" ||
-        this.rank == "7" || this.rank == "8" || this.rank == "9" ||
-        this.rank == "10") {
-        spotNode.className = "spotA1";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotA5";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotC1";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotC5";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-    if (this.rank == "6" || this.rank == "7" || this.rank == "8") {
-        spotNode.className = "spotA3";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotC3";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-    if (this.rank == "7" || this.rank == "8" || this.rank == "10") {
-        spotNode.className = "spotB2";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-    if (this.rank == "8" || this.rank == "10") {
-        spotNode.className = "spotB4";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-    if (this.rank == "9" || this.rank == "10") {
-        spotNode.className = "spotA2";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotA4";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotC2";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotC4";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-
-    // For face cards (Jack, Queen or King), create and add the proper image.
-
-    tempNode = document.createElement("IMG");
-    tempNode.className = "face";
-    if (this.rank == "J")
-        tempNode.src = "graphics/jack.gif";
-    if (this.rank == "Q")
-        tempNode.src = "graphics/queen.gif";
-    if (this.rank == "K")
-        tempNode.src = "graphics/king.gif";
-
-    // For face cards, add suit characters to the upper-left and lower-right
-    // corners.
-
-    if (this.rank == "J" || this.rank == "Q" || this.rank == "K") {
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotA1";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-        spotNode.className = "spotC5";
-        tempNode = spotNode.cloneNode(true);
-        frontNode.appendChild(tempNode);
-    }
-
-    // Add front node to the card node.
-
-    cardNode.appendChild(frontNode);
-
-    // Return the card node.
-
-    return cardNode;
-}
+//function cardCreateNode() {
+//
+//    var cardNode, frontNode, indexNode, spotNode, tempNode, textNode;
+//    var indexStr, spotChar;
+//
+//    // This is the main node, a DIV tag.
+//
+//    cardNode = document.createElement("DIV");
+//    cardNode.className = "card";
+//
+//    // Build the front of card.
+//
+//    frontNode = document.createElement("DIV");
+//    frontNode.className = "front";
+//
+//    // Get proper character for card suit and change font color if necessary.
+//
+//    spotChar = "\u00a0";
+//    switch (this.suit) {
+//        case "C":
+//            spotChar = "\u2663";
+//            break;
+//        case "D":
+//            frontNode.className += " red";
+//            spotChar = "\u2666";
+//            break;
+//        case "H":
+//            frontNode.className += " red";
+//            spotChar = "\u2665";
+//            break;
+//        case "S":
+//            spotChar = "\u2660";
+//            break;
+//    }
+//
+//    // Create and add the index (rank) to the upper-left corner of the card.
+//
+//    indexStr = this.rank;
+//    if (this.toString() == "")
+//        indexStr = "\u00a0";
+//    spotNode = document.createElement("DIV");
+//    spotNode.className = "index";
+//    textNode = document.createTextNode(indexStr);
+//    spotNode.appendChild(textNode);
+//    spotNode.appendChild(document.createElement("BR"));
+//    textNode = document.createTextNode(spotChar);
+//    spotNode.appendChild(textNode);
+//    frontNode.appendChild(spotNode);
+//
+//    // Create and add spots based on card rank (Ace thru 10).
+//
+//    spotNode = document.createElement("DIV");
+//    textNode = document.createTextNode(spotChar);
+//    spotNode.appendChild(textNode);
+//    if (this.rank == "A") {
+//        spotNode.className = "ace";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//    if (this.rank == "3" || this.rank == "5" || this.rank == "9") {
+//        spotNode.className = "spotB3";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//    if (this.rank == "2" || this.rank == "3") {
+//        spotNode.className = "spotB1";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//    if (this.rank == "2" || this.rank == "3") {
+//        spotNode.className = "spotB5";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//    if (this.rank == "4" || this.rank == "5" || this.rank == "6" ||
+//        this.rank == "7" || this.rank == "8" || this.rank == "9" ||
+//        this.rank == "10") {
+//        spotNode.className = "spotA1";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotA5";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotC1";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotC5";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//    if (this.rank == "6" || this.rank == "7" || this.rank == "8") {
+//        spotNode.className = "spotA3";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotC3";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//    if (this.rank == "7" || this.rank == "8" || this.rank == "10") {
+//        spotNode.className = "spotB2";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//    if (this.rank == "8" || this.rank == "10") {
+//        spotNode.className = "spotB4";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//    if (this.rank == "9" || this.rank == "10") {
+//        spotNode.className = "spotA2";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotA4";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotC2";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotC4";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//
+//    // For face cards (Jack, Queen or King), create and add the proper image.
+//
+//    tempNode = document.createElement("IMG");
+//    tempNode.className = "face";
+//    if (this.rank == "J")
+//        tempNode.src = "graphics/jack.gif";
+//    if (this.rank == "Q")
+//        tempNode.src = "graphics/queen.gif";
+//    if (this.rank == "K")
+//        tempNode.src = "graphics/king.gif";
+//
+//    // For face cards, add suit characters to the upper-left and lower-right
+//    // corners.
+//
+//    if (this.rank == "J" || this.rank == "Q" || this.rank == "K") {
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotA1";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//        spotNode.className = "spotC5";
+//        tempNode = spotNode.cloneNode(true);
+//        frontNode.appendChild(tempNode);
+//    }
+//
+//    // Add front node to the card node.
+//
+//    cardNode.appendChild(frontNode);
+//
+//    // Return the card node.
+//
+//    return cardNode;
+//}
 
 //=============================================================================
 // Stack Object
@@ -377,9 +548,10 @@ function stackDraw(n) {
 // stackAdd(card): Adds the given card to the stack.
 //-----------------------------------------------------------------------------
 
-function stackAddCard(card) {
-
-    this.cards.push(card);
+function stackAddCard(card, array) {
+    array.push(card);
+    //this.cards.push(card);
+    //added second argument array
 }
 
 //-----------------------------------------------------------------------------
@@ -401,42 +573,3 @@ function stackCardCount() {
 
     return this.cards.length;
 }
-
-
-////////// CUSTOM FUNCTIONS
-
-function startModal() {
-    $('#start-modal').foundation('reveal', 'open');
-    $("#start-modal .button").on('click', function () {
-        var numberOfDecks = $("#number-of-decks").val();
-        $('#start-modal').foundation('reveal', 'close');
-
-    });
-    return numberOfDecks
-}
-
-
-
-
-
-//TEST INIT
-
-$(document).ready(function () {
-    startModal();
-    stackMakeDeck(startModal());
-    
-
-
-
-
-
-    var $dealerImg = $("<img>");
-    $dealerImg.attr("src", cards[93].image);
-    $dealerImg.attr("style", "max-height: 200px;");
-    $('#dealer-area').append($dealerImg);
-
-    var $playerImg = $("<img>");
-    $playerImg.attr("src", cards[25].image);
-    $playerImg.attr("style", "max-height: 200px;");
-    $('#player-area').append($playerImg);
-});
